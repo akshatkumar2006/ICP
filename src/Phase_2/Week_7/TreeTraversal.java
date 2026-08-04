eb20628e46f411891c9454f2c2bb31abf517a7a0
Ayush Chaudhary <147637466+ayush23chaudhary@users.noreply.github.com>
2026-08-04 15:25:45 +0530

    more practice

---
+++ src/Phase_2/Week_7/TreeTraversal.java
@@ -0,0 +1,14 @@
+package Phase_2.Week_7;
+
+public class TreeTraversal {
+
+    public static void main(String[] args) {
+
+    }
+
+    class Tree{
+        class Node{
+
+        }
+    }
+}
---
+++ src/Phase_2/Week_9/MinimumTimeToVisitDisappearingNodes.java
@@ -0,0 +1,12 @@
+package Phase_2.Week_9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumTimeToVisitDisappearingNodes {
+    class Solution {
+        public int[] minimumTime(int n, int[][] edges, int[] disappear) {
+            return new int[]{};
+        }
+    }
+}
