072a08e0125bd2d58f5cd49c47ac1dcab8495fd9
akshatkumar2006 <masterak2443@gmail.com>
2026-09-07 11:45:36 +0530

    test commit

--- src/Phase_3/Day_2/EqualSubsetSum.java
+++ src/Phase_3/Day_2/EqualSubsetSum.java
@@ -1,8 +1,7 @@
 package Phase_3.Day_2;
 
 public class EqualSubsetSum {
-    public static void main(String[] args) {
-	// JAVA Code for equal subset sum        
+    public static void main(String[] args) {       
 int[] arr = {2, 3, 5, 11, 1};
         int sum = 0;
         for(int ele : arr){
